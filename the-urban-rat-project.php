3fc88f1814276b2f7178aeff0f2b71262663c837
Michael Fielding <michael.fielding@microneer.com>
2019-07-29 22:24:22 +1200

    Require user names to register, and show suggestion to go to /join

--- the-urban-rat-project.php
+++ the-urban-rat-project.php
@@ -68,4 +68,58 @@ if ( is_admin() ){
 else // not in the backend admin section
 {
 	
+	// Require user's first and last names when they register, and show a message about not needing an 
+	// account to receive reminders.
+	
+	//1. Add a new form element...
+	add_action( 'register_form', 'turp_register_form' );
+	function turp_register_form() {
+
+		$first_name = ( ! empty( $_POST['first_name'] ) ) ? sanitize_text_field( $_POST['first_name'] ) : '';
+		$last_name = ( ! empty( $_POST['first_name'] ) ) ? sanitize_text_field( $_POST['first_name'] ) : '';
+			
+		?>
+		<p>
+			<label for="first_name"><?php _e( 'First Name', TURP_TEXT_DOMAIN ) ?><br />
+				<input type="text" name="first_name" id="first_name" class="input" value="<?php echo esc_attr(  $first_name  ); ?>" size="25" /></label>
+		</p>
+		<p>
+			<label for="last_name"><?php _e( 'Last Name', TURP_TEXT_DOMAIN ) ?><br />
+				<input type="text" name="last_name" id="last_name" class="input" value="<?php echo esc_attr(  $last_name  ); ?>" size="25" /></label>
+		</p>
+		<?php
+	
+		// add in a message about not using Register to get reminders
+		$content = ob_get_contents();
+		$content = str_replace( 'Register For This Site', 'If you want to receive reminders and file reports then don\'t register for a full project administrator account here - instead <a href="/join">sign up for reminders</a>.', $content );
+		ob_get_clean();
+		echo $content;
+    }
+
+    //2. Add validation. In this case, we make sure first_name is required.
+    add_filter( 'registration_errors', 'turp_registration_errors', 10, 3 );
+    function turp_registration_errors( $errors, $sanitized_user_login, $user_email ) {
+        
+        if ( empty( $_POST['first_name'] ) || ! empty( $_POST['first_name'] ) && trim( $_POST['first_name'] ) == '' ) {
+        $errors->add( 'first_name_error', sprintf('<strong>%s</strong>: %s',__( 'ERROR', TURP_TEXT_DOMAIN ),__( 'You must include a first name.', TURP_TEXT_DOMAIN ) ) );
+
+        }
+        if ( empty( $_POST['last_name'] ) || ! empty( $_POST['last_name'] ) && trim( $_POST['last_name'] ) == '' ) {
+        $errors->add( 'last_name_error', sprintf('<strong>%s</strong>: %s',__( 'ERROR', TURP_TEXT_DOMAIN ),__( 'You must include a last name.', TURP_TEXT_DOMAIN ) ) );
+
+        }
+
+        return $errors;
+    }
+
+    //3. Finally, save our extra registration user meta.
+    add_action( 'user_register', 'turp_user_register' );
+    function turp_user_register( $user_id ) {
+        if ( ! empty( $_POST['first_name'] ) ) {
+            update_user_meta( $user_id, 'first_name', sanitize_text_field( $_POST['first_name'] ) );
+        }
+        if ( ! empty( $_POST['last_name'] ) ) {
+            update_user_meta( $user_id, 'last_name', sanitize_text_field( $_POST['last_name'] ) );
+        }
+    }
 }
